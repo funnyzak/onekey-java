a7d9777cfa651fb7ef63a4e0863dafd367f947cc
Leon <silenceace@gmail.com>
2021-11-29 14:55:30 +0800

    refactor: zip utils

--- onekey-common/src/main/java/com/github/funnyzak/onekey/common/utils/ZipUtils.java
+++ onekey-common/src/main/java/com/github/funnyzak/onekey/common/utils/ZipUtils.java
@@ -8,17 +8,16 @@ import org.apache.commons.compress.archivers.zip.ZipArchiveInputStream;
 import org.apache.commons.compress.compressors.bzip2.BZip2CompressorInputStream;
 import org.apache.commons.compress.compressors.gzip.GzipCompressorInputStream;
 import org.apache.tomcat.util.http.fileupload.IOUtils;
-import org.nutz.lang.Lang;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
 import java.io.*;
 import java.nio.charset.StandardCharsets;
 import java.nio.file.Files;
+import java.nio.file.Paths;
 import java.util.ArrayList;
 import java.util.Enumeration;
 import java.util.List;
-import java.util.regex.Matcher;
 import java.util.zip.GZIPOutputStream;
 import java.util.zip.ZipEntry;
 import java.util.zip.ZipFile;
@@ -129,6 +128,9 @@ public class ZipUtils {
         }
 
         outputDir = outputDir.endsWith(File.separator) ? outputDir : outputDir + File.separator;
+
+        createDirectory(outputDir, null);
+
         ZipArchiveInputStream is = null;
         List<String> fileNames = new ArrayList<String>();
 
@@ -137,17 +139,15 @@ public class ZipUtils {
             ZipArchiveEntry entry = null;
 
             while ((entry = is.getNextZipEntry()) != null) {
-                fileNames.add(entry.getName());
-
-                if (entry.isDirectory()) {
-                    File directory = new File(outputDir, entry.getName());
-                    directory.mkdirs();
-                } else {
-                    checkParentDirectoryExist(outputDir, entry.getName());
+                createDirectory(outputDir, entry.getName());
+                if (!entry.isDirectory()) {
+                    fileNames.add(entry.getName());
                     OutputStream os = null;
                     try {
                         os = new BufferedOutputStream(new FileOutputStream(new File(outputDir, entry.getName())), BUFFER_SIZE);
                         IOUtils.copy(is, os);
+                    } catch (Exception ex) {
+                        logger.error("解压缩文件失败, Err => {}", ex);
                     } finally {
                         IOUtils.closeQuietly(os);
                     }
@@ -164,31 +164,6 @@ public class ZipUtils {
     }
 
     /**
-     * 检查当前路径中，最后一个文件之前的父级目录是否存在
-     *
-     * @param outputDir 输出的父路径
-     * @param path      当前生成的文件路径
-     */
-    private static void checkParentDirectoryExist(String outputDir, String path) {
-        String[] fileNames = path.split(Lang.isWin() ? "/" : File.separator);
-        if (fileNames.length <= 1) {
-            return;
-        }
-        StringBuffer filePath = new StringBuffer(outputDir);
-        for (int index = 0; index < fileNames.length - 1; index++) {
-            if (index != 0) {
-                filePath.append(Lang.isWin() ? Matcher.quoteReplacement(File.separator) : File.separator);
-            }
-            filePath.append(fileNames[index]);
-            File directory = new File(filePath.toString());
-            if (!directory.exists()) {
-                directory.mkdir();
-            }
-        }
-
-    }
-
-    /**
      * 解压 .zip 文件（内置解压，不支持中文）
      *
      * @param file      要解压的zip文件对象
@@ -202,13 +177,12 @@ public class ZipUtils {
             Enumeration<?> enums = zipFile.entries();
             while (enums.hasMoreElements()) {
                 ZipEntry entry = (ZipEntry) enums.nextElement();
-                if (entry.isDirectory()) {
-                    //创建空目录
+                createDirectory(outputDir, entry.getName());
+                if (!entry.isDirectory()) {
                     createDirectory(outputDir, entry.getName());
-                } else {
                     try (InputStream in = zipFile.getInputStream(entry)) {
                         try (OutputStream out = new FileOutputStream(
-                                new File(outputDir + File.separator + entry.getName()))) {
+                                new File(Paths.get(outputDir, entry.getName()).toUri()))) {
                             writeFile(in, out);
                         }
                     }
@@ -226,11 +200,8 @@ public class ZipUtils {
             createDirectory(outputDir, null);
             TarArchiveEntry entry = null;
             while ((entry = tarIn.getNextTarEntry()) != null) {
-                //是目录
-                if (entry.isDirectory()) {
-                    //创建空目录
-                    createDirectory(outputDir, entry.getName());
-                } else {
+                createDirectory(outputDir, entry.getName());
+                if (!entry.isDirectory()) {
                     //是文件
                     try (OutputStream out = new FileOutputStream(
                             new File(outputDir + File.separator + entry.getName()))) {
@@ -256,9 +227,8 @@ public class ZipUtils {
             createDirectory(outputDir, null);
             TarArchiveEntry entry;
             while ((entry = tarIn.getNextTarEntry()) != null) {
-                if (entry.isDirectory()) {
-                    createDirectory(outputDir, entry.getName());
-                } else {
+                createDirectory(outputDir, entry.getName());
+                if (!entry.isDirectory()) {
                     try (OutputStream out = new FileOutputStream(
                             new File(outputDir + File.separator + entry.getName()))) {
                         writeFile(tarIn, out);
@@ -293,13 +263,16 @@ public class ZipUtils {
         File file = new File(outputDir);
         //子目录不为空
         if (!(subDir == null || subDir.trim().equals(""))) {
-            file = new File(outputDir + File.separator + subDir);
+            file = new File(Paths.get(outputDir, subDir).toUri());
         }
         if (!file.exists()) {
             if (!file.getParentFile().exists()) {
                 file.getParentFile().mkdirs();
             }
-            file.mkdirs();
+
+            if (file.isDirectory()) {
+                file.mkdirs();
+            }
         }
     }
 
